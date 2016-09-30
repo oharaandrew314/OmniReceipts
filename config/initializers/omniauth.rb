047336c53740ae1509e31860ef624289972d9e20
oharaandrew314 <ohara.andrew314@gmail.com>
2016-09-30 00:02:54 +0000

    get google client id and secret from secrets.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'omniauth-google-oauth2'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
+  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret, {
     :scope => [ 'profile', 'drive' ],
   }
 end
