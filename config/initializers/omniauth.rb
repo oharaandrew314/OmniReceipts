a83ef21ceca0445bc5a66ef40b6b02a81dd34ebf
oharaandrew314 <ohara.andrew314@gmail.com>
2016-10-20 18:57:21 +0000

    always ask user for account when signing in

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,9 @@ require 'omniauth-google-oauth2'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Figaro.env.google_client_id, Figaro.env.google_client_secret, {
-    :scope => [ 'profile' ],
     :image_aspect_ratio => "square",
     :image_size => 50,
-    :access_type => "online"
+    :access_type => "online",
+    :prompt => 'select_account'
   }
 end
